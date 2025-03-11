15a1e489662627beea59e9887d95043435c07817
Liene31 <67922137+Liene31@users.noreply.github.com>
2025-03-11 17:06:17 +0100

    Initialised App + Database

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+} from "https://www.gstatic.com/firebasejs/11.4.0/firebase-database.js";
+
+const appSettings = {
+  databaseURL:
+    "https://playground-e8baf-default-rtdb.europe-west1.firebasedatabase.app/",
+};
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDB = ref(database, "shoppingList");
+
 const inputEl = document.querySelector("#input-el");
 const addBtn = document.querySelector("#add-btn");
 const listEl = document.querySelector("#ulEL");
