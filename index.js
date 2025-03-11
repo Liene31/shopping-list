a01311f214549e7b885cc1add2d65de2ef2c6fa3
Liene31 <67922137+Liene31@users.noreply.github.com>
2025-03-11 22:34:23 +0100

    Updated onValue + added remove()

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import {
   ref,
   push,
   onValue,
+  remove,
 } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-database.js";
 
 const appSettings = {
@@ -19,15 +20,31 @@ const addBtn = document.querySelector("#add-btn");
 const listEl = document.querySelector("#ulEL");
 
 function appendListItems(item) {
-  listEl.innerHTML += `<li>${item}</li>`;
+  const itemValue = item[1];
+  const itemID = item[0];
+
+  const newEl = document.createElement("li");
+  newEl.textContent = itemValue;
+
+  newEl.addEventListener("click", function () {
+    let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`);
+    remove(exactLocationOfItemInDB);
+  });
+
+  listEl.append(newEl);
 }
 
 onValue(shoppingListInDB, function (snapshot) {
-  let itemArray = Object.values(snapshot.val());
-  listEl.innerHTML = "";
+  if (snapshot.exists()) {
+    let itemArray = Object.entries(snapshot.val());
+    listEl.innerHTML = "";
 
-  for (let i = 0; i < itemArray.length; i++) {
-    appendListItems(itemArray[i]);
+    for (let i = 0; i < itemArray.length; i++) {
+      let itemEntries = itemArray[i];
+      appendListItems(itemEntries);
+    }
+  } else {
+    listEl.innerHTML = "No items here... yet";
   }
 });
 
